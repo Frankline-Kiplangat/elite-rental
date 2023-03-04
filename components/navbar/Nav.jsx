bb7b3b2cfb0ea4ca43938aec4aeeb1e576c8ae4d
Frankline-Kiplangat <kipfrankline@gmail.com>
2023-03-04 14:12:12 +0300

    Add navbar and navbar menu

---
+++ components/navbar/Nav.jsx
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { GlobeAltIcon, SearchIcon } from "@heroicons/react/solid";
+import "react-date-range/dist/styles.css"; // main style file for date picker
+import "react-date-range/dist/theme/default.css"; // theme css file date picker
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
+import { useAuth } from "../../contexts/AuthContext";
+import logo from "../../public/images/logo1.jpeg";
+import WarningModel from "../modals/WarningModal";
+import Menu from "./Menu";
+     
+function Nav({ placeholder }) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [warningContent, setWarningContent] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const router = useRouter();
+  const { currentUser } = useAuth();
+
+
+  const dateRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  /* DATE SELECTION */
+  const handleDatePicker = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+    console.log(ranges.selection.endDate)
+  };
+  /* CLEAR SEACRH INPUT */
+  const resetSearchInput = () => {
+    setSearchInput("");
+  };
+
+  const search = () => {
+    /* Check if user authenticated */
+    // if (currentUser) {
+      /* search input validation */
+      if (searchInput?.length > 2 && searchInput?.charAt(0) != " ") {
+        if (startDate && endDate >= new Date()) {
+          if (
+            searchInput?.toLowerCase() === "Nairobi" ||
+            searchInput?.toLowerCase() === "Kisumu" ||
+            searchInput?.toLowerCase() === "Nakuru" ||
+            searchInput?.toLowerCase() === "Eldoret" 
+           
+          ) {
+            router.push({
+              pathname: "/search",
+              query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+              },
+            });
+            setTimeout(() => {
+              if (router.pathname.includes("search")) router.reload();
+            });
+          } else {
+            router.push("/NotFound");
+          }
+        } else {
+          setOpen(true); 
+          setWarningContent("Date must be greater than today's date.");
+        }
+      } else {
+        setOpen(true); 
+        setWarningContent("Please enter a valid city.");
+      }
+ 
+  };
+
+  return (
+    <>
+      <header
+        className="sticky top-0 z-50 flex md:grid md:grid-cols-3 bg-white shadow-md 
+      p-4 md:px-10 justify-between "
+      >
+        <div className="relative flex items-center h-7 md:h-12 my-auto">
+          <Image
+            src={logo}
+            alt="logo"
+            width={180}
+            height={80}
+            objectFit="contain"
+            objectPosition="left"
+            className="cursor-pointer"
+            onClick={() => router.push("/")}
+          />
+        </div>
+
+        <div
+          className="flex items-center ml-2 sm:border-2 md:inline-flex 
+                rounded-full py-2 md:shadow-sm"
+        >
+          <input
+            value={searchInput}
+            onChange={(event) => setSearchInput(event.target.value)}
+            type="text"
+            className="flex-grow md:pl-5 ml-1 w-full border-2 py-1 sm:border-none rounded-full bg-transparent outline-none
+               text-gray-600 placeholder-gray-500"
+            placeholder={placeholder || "Search available cars"}
+          />
+          <SearchIcon
+            onClick={search}
+            className="h-8 w-8 hidden lg:inline-flex bg-blue-400 text-white rounded-full p-2 cursor-pointer md:mx-2"
+          />
+        </div>
+        <div className="flex items-center justify-end text-gray-500 space-x-4">
+          <div
+            onClick={() => {
+              currentUser
+                ? router.push("/auth/ListCar")
+                : router.push("/auth/LoginPage");
+            }}
+          >
+            <p className="hidden lg:inline cursor-pointer">Join</p>
+
+            <GlobeAltIcon className="h-6 ml-2 mb-0.5 cursor-pointer hidden lg:inline" />
+          </div>
+
+          <Menu />
+        </div>
+      </header>
+      <div className="flex justify-center align-middle">
+        {searchInput && (
+          <div className="flex flex-col col-span-3 mb-4">
+            <DateRangePicker
+              ranges={[dateRange]}
+              minDate={new Date()}
+              rangeColors={["#3f97f2"]}
+              onChange={handleDatePicker}
+              scroll={{ enabled: false }}
+            />
+
+            <div className="flex justify-center align-middle sm:w-full ">
+              <button
+                onClick={resetSearchInput}
+                className="flex-grow relative text-gray-500 font-semibold py-1 border-2 rounded-full m-2 hover:shadow-sm
+                hover:bg-gray-100"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={search}
+                className="flex-grow text-blue-500 font-semibold py-1 border-2 rounded-full m-2 hover:shadow-sm
+                hover:bg-gray-100"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        )}
+        {open && (
+          <WarningModel
+            setOpen={setOpen}
+            content={warningContent}
+            setWarningContent={setWarningContent}
+          />
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Nav;
