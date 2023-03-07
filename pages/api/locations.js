9cdb491441490179b00ad15d2d871041325077eb
Frankline-Kiplangat <kipfrankline@gmail.com>
2023-03-07 09:52:10 +0300

    Add locations endpoints api

---
+++ pages/api/locations.js
@@ -0,0 +1,16 @@
+[{
+    "country": "Kenya",
+    "city": "Nairobi",
+    "image": "../public/images/nai.jpeg",
+    "alt": "Nairobi Image"
+}, {
+    "country": "Kenya",
+    "city": "Kisumu",
+    "image": "../public/images/kism.jpeg",
+    "alt": "Kisumu Image"
+}, {
+    "country": "Kenya",
+    "city": "Nakuru",
+    "image": "../public/images/naks.jpeg",
+    "alt": "Nakuru Image"
+}]
